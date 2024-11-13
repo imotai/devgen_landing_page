595e0818b5abc65c2f9e3f1146fe96af9f103af2
imotai <codego.me@gmail.com>
2024-11-13 21:56:51 +0800

    feat: add loading effect

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,7 +1,7 @@
 import ReactPlayer from 'react-player'
 import React, { useState, useEffect, useRef } from 'react'
 import { CirclePlay } from 'lucide-react'
-import { AspectRatio, Card } from '@mui/joy'
+import { AspectRatio, Card, CircularProgress, Stack } from '@mui/joy'
 
 type PlayerProps = {
     url: string
@@ -12,8 +12,29 @@ export default function Player(props: PlayerProps) {
     const { url, light } = props
     const player = useRef(null)
     const [playing, setPlaying] = useState(false)
+    const [isLoading, setIsLoading] = useState(false)
     return (
         <Card color="neutral" variant="outlined">
+            {
+                isLoading && (
+                    <Stack
+                        sx={{
+                            position: 'absolute',
+                            top: 0,
+                            left: 0,
+                            right: 0,
+                            bottom: 0,
+                            display: 'flex',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            backgroundColor: 'rgba(0,0,0,0.5)',
+                            zIndex: 1000
+                        }}
+                    >
+                        <CircularProgress/>
+                    </Stack>
+                )
+            }
             <AspectRatio
                 sx={{
                     width: '100%',
@@ -25,6 +46,15 @@ export default function Player(props: PlayerProps) {
                     url={url}
                     light={light}
                     playing={playing}
+                    onReady={() => {
+                        setIsLoading(false)
+                    }}
+                    onBuffer={() => {
+                        setIsLoading(true)
+                    }}
+                    onBufferEnd={() => {
+                        setIsLoading(false)
+                    }}
                     width={'100%'}
                     height={'100%'}
                     onEnded={() => {
